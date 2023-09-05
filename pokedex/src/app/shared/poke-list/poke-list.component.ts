117fe2b5a338366d14202949c8d658daf9978d31
Pedro_Henrique <phsouzahd15@gmail.com>
2023-09-05 14:01:01 -0300

    Update poke-list.component.ts

--- pokedex/src/app/shared/poke-list/poke-list.component.ts
+++ pokedex/src/app/shared/poke-list/poke-list.component.ts
@@ -8,13 +8,17 @@ import { PokeApiService } from 'src/app/service/poke-api.service';
 })
 export class PokeListComponent implements OnInit{
 
+  public getAllPokemon: any;
   constructor(
     private pokeApiService: PokeApiService
   ){}
 
   ngOnInit():void{
     this.pokeApiService.apiListAllPokemons.subscribe(
-      res => console.log(res)
+      res => {
+        this.getAllPokemon = res.results;
+        console.log(this.getAllPokemon);
+      }
     );
   }
 }
